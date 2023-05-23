a8f0204aece23dd662dacefdc752d7a6513d1daf
paulasostaric <85227409+paulasostaric@users.noreply.github.com>
2023-05-23 20:12:06 +0200

    didfjhfrhdhedhes
    dADWFGT3

--- app/src/main/java/com/example/projektniniop/SettingsActivity.kt
+++ app/src/main/java/com/example/projektniniop/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.projektniniop
 
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projektniniop.HomeActivity
 import com.example.projektniniop.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,5 +36,15 @@ class SettingsActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+    @Suppress("DEPRECATION")
+    fun changeLanguage(context: Context, language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val res = context.resources
+        val config = Configuration(res.configuration)
+        config.setLocale(locale)
+        context.createConfigurationContext(config)
+        res.updateConfiguration(config, res.displayMetrics)
     }
 }
